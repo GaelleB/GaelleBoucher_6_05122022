fc1bc4d2bd6636541c485210e186fddfc6df202b
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-12-07 16:42:33 +0100

    modif du factory photographer.js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,29 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, id, price, tagline } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const location = document.createElement('p');
+        const tag = document.createElement('p');
+        const prix = document.createElement('p');
+
+        img.setAttribute("src", picture);
+        h2.innerText = name;
+
+        location.classList.add("location");
+        location.innerText = `${city}, ${country}`;
+
+        tag.classList.add("tagline");
+        tag.innerText = data.tagline;
+
+        prix.classList.add("price");
+        prix.innerText = `${price}€/jour`;
+        
+        article.append(img, h2, location, tag, prix);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, tagline, getUserCardDOM }
 }
