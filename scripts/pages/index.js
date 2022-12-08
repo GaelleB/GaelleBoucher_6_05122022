f7bf1b6014d3dca2aa25bb00831ec11f011b32b4
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-12-08 17:28:35 +0100

    lien entre la page d'accueuil et la page photographe avec l'id en url

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,21 +1,28 @@
+    // Fonction pour récupérer les données des photographes
     async function getPhotographers() {
         return await (await fetch('../../data/photographers.json')).json();
     }
 
+    // Fonction pour afficher les photographes sur la page d'accueuil
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
-            // console.log(photographers);
+
+            const link = document.createElement('a');
+            link.setAttribute('href', `photographer.html?id=${photographer.id}`);
+            link.setAttribute('title', `Lien vers ${photographer.name}`);
+            link.style.textDecoration = 'none';
+            link.appendChild(userCardDOM);
+            photographersSection.appendChild(link);
         });
     };
 
+    // Fonction pour afficher les photographes sur la page d'accueuil au moment du chargement de la page
     async function init() {
-        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
     init();
-    
