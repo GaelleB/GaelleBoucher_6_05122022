9e7d7f38e68ffe2827393ef887dabd3539cb7848
Gaelle Boucher <gaelleboucher23@gmail.com>
2022-12-14 18:28:48 +0100

    page photographe

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,6 +5,7 @@
 
     // Fonction pour afficher les photographes sur la page d'accueuil
     async function displayData(photographers) {
+        console.log(photographers);
         const photographersSection = document.querySelector(".photographer_section");
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,20 +1,31 @@
     // Fonction pour récupérer les données des photographes
     async function getPhotographers() {
-        return await (await fetch('../../data/photographers.json')).json();
+        const response = await fetch("../../data/photographers.json")
+        const photographersData = await response.json()
+        console.log(photographersData)
+        return photographersData
+    }
+
+    function getPhotographerId(){
+        const url = new URL(window.location)
+        const photographerId = parseInt(url.searchParams.get("id"), 10)
+        return photographerId
     }
 
     // Fonction pour afficher les photographes sur la page photographe
     async function displayData(photographer) {
         const photographerHeader = document.querySelector('.photograph-header');
         const btnContact = document.querySelector('.contact_button');
-                const photographerModel = photographerFactory(photographer);
-                const sectionHeader = photographerModel.photographerProfile();
-                photographerHeader.insertBefore(sectionHeader, btnContact);
+        const photographerModel = photographerFactory(photographer);
+        const sectionHeader = photographerModel.photographerProfile();
+        photographerHeader.insertBefore(sectionHeader, btnContact);
     };
 
     // Fonction pour afficher les photographes sur la page photographe au moment du chargement de la page
     async function init() {
-        const { photographer } = await getPhotographers();
-        displayPhotographe(photographer);
+        const { photographers } = await getPhotographers();
+        const photographerId = getPhotographerId()
+        const foundPhotographer = photographers.find((photographer) => photographer.id === photographerId)
+        displayData(foundPhotographer);
     };
     init();
