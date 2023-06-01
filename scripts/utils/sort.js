ae824d7a4983dfddea0012c0e7726bc847afdfd1
Gaelle Boucher <gaelleboucher23@gmail.com>
2023-06-01 22:42:44 +0200

    modif kif icon + début tri

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -1,9 +1,5 @@
 const menuSelect = document.getElementById('sort-medias');
 menuSelect.addEventListener('change', function() {
-    const photographerMedias = [photographerMedias];
-    displayDataMedias();
-    displayLightboxMedias(photographerMedias);
-    photographerMedias.sort()
+    
 });
 console.log(menuSelect.value);
-console.log(photographerMedias);
