c79bf7d634c1d5b9bf2c7309c1ec28da933d3df6
Gaelle Boucher <gaelleboucher23@gmail.com>
2023-06-01 15:57:05 +0200

    modif html + css et début contactForm.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,9 @@
+const openFormButton = document.querySelector(".contact_button");
+openFormButton.addEventListener("click", displayModal)
+
+const closeFormButton = document.querySelector(".modal .close_modal");
+closeFormButton.addEventListener("click", closeModal)
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
